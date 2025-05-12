37c4625dbac33952f791327b9116b97a65be9adf
Abdelrahman ELsokary <abdalrahmanelsokarey@gmail.com>
2025-05-12 23:19:25 +0300

    Update: swiper.js

--- assets/scripts/libs/swiper.js
+++ assets/scripts/libs/swiper.js
@@ -1,31 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.mySwiper', {
-    // تكرار السلايدر (Loop)
     loop: true,
 
-    // أزرار التنقل
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     },
 
-    // نقاط التقسيم
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     },
 
-    // التمرير التلقائي (اختياري)
+
     autoplay: {
       delay: 3000,
       disableOnInteraction: false
     },
 
-    // عدد الشرائح المعروضة (اختياري)
     slidesPerView: 1,
     spaceBetween: 20,
 
-    // تكيف مع الشاشات الصغيرة (Responsive)
     breakpoints: {
       768: {
         slidesPerView: 2
